cfbf2ebbb49e2b4e4716ea048442e147e077cfe0
Piske <marcelopiske@ideyas.com>
2023-09-02 02:23:49 -0300

    feat: subindo novo codigo com, com bug de previus e next

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const flipper_container = document.getElementById('container-1')
 const flipper = document.getElementById('flipper-1')
 const next = document.getElementById('next')
+const previus = document.getElementById('previus')
 
 flipper_container.addEventListener('click', () => {
     if (flipper.style.transform != "rotateY(180deg)") {
@@ -9,13 +10,23 @@ flipper_container.addEventListener('click', () => {
         flipper.style.transform = 'rotateY(0deg)'
     }
 })
-next.addEventListener('click', () => {
+
+function greek_card_animation() {
     if (flipper.style.transform != "rotateX(360deg)") {
         flipper.style.transform = 'rotateX(360deg)'
     }else{
         flipper.style.transform = 'rotateX(0deg)'
     }
-    alterar_palavra()
+}
+
+next.addEventListener('click', () => {
+    greek_card_animation()
+    alterar_palavra(true)
+})
+
+previus.addEventListener('click', () => {
+    greek_card_animation()
+    alterar_palavra(false)
 })
 
 p_greek = document.getElementById("text-greek")
@@ -31,17 +42,28 @@ function set_greek_word(jsondata) {
     p_portuguese.innerText = text_portuguese
 }
 
-function alterar_palavra() { fetch("greek_words.json")
+function set_local_word_id(next) {
+    const atual_word_id = localStorage.getItem("word_id")
+    let next_word_id = parseInt(atual_word_id)
+    
+    if (next == false)
+        next_word_id = parseInt(atual_word_id) - 1
+    else
+        next_word_id = parseInt(atual_word_id) + 1
+    
+    localStorage.setItem("word_id", next_word_id);
+}
+
+function alterar_palavra(next) { 
+    fetch("greek_words.json")
     .then(response => {
         return response.json();
     })
     .then(jsondata => {
-        const atual_word_id = localStorage.getItem("word_id")
-        let next_word_id = parseInt(atual_word_id) + 1
-        localStorage.setItem("word_id", next_word_id);
+        set_local_word_id(next)
         set_greek_word(jsondata)
     })
 }
 
-
 localStorage.setItem("word_id", "0");
+alterar_palavra(null)

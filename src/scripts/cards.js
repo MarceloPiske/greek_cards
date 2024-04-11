7ab185723ce1336a10e47507aa5f285cf61537ec
Piske <marcelohpiske@hotmail.com>
2024-04-11 20:13:19 -0300

    subindo novas funcionalidades e novas palavras

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,8 +1,169 @@
 const card_container = document.getElementById('card-container')
 const next = document.getElementById('next')
 const previus = document.getElementById('previus')
-let greek_words = []
 let atual_card_id = 1
+
+//IndexDB
+//db.deleteObjectStore('books')
+
+function abrirBancoDeDados() {
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open("greek_words", 1);
+
+        request.onerror = function (event) {
+            reject("Erro ao abrir o banco de dados: " + event.target.error);
+        };
+
+        request.onsuccess = function (event) {
+            const db = event.target.result;
+            resolve(db);
+        };
+
+        request.onupgradeneeded = function (event) {
+            const db = event.target.result;
+
+            const objectStore = db.createObjectStore("words", { keyPath: "id" });
+            objectStore.createIndex("id", "id", { unique: true });
+            objectStore.createIndex("Vocabulo", "Vocabulo", { unique: false });
+            objectStore.createIndex("Artigo_Definido", "Artigo_Definido", { unique: false });
+            objectStore.createIndex("Classificacao", "Classificacao", { unique: false });
+            objectStore.createIndex("Casos", "Casos", { unique: false });
+            objectStore.createIndex("Decorado", "Decorado", { unique: false });
+        };
+    });
+}
+
+// Função para inserir dados
+async function inserirDados(jsondata) {
+    const db = await abrirBancoDeDados();
+
+    const transaction = db.transaction(["words"], "readwrite");
+    const store = transaction.objectStore("words");
+
+    for (let word of jsondata) {
+        const request = store.get(1);
+        request.onsuccess = function (event) {
+            const pessoa = event.target.result;
+            if (pessoa) {
+                //console.log("Dado encontrado:", pessoa);
+            } else {
+                console.log("Dado não encontrado.");
+                let request = store.add(word);
+                request.onsuccess = function (event) {
+                    console.log("Dados inseridos com sucesso!");
+                };
+
+                request.onerror = function (event) {
+                    console.error(word, "Erro ao inserir dados:", event.target.error);
+                };
+            }
+        };
+    }
+}
+
+// Função para atualizar dados
+async function atualizarDados(id, decorado) {
+    const db = await abrirBancoDeDados();
+
+    const transaction = db.transaction(["words"], "readwrite");
+    const store = transaction.objectStore("words");
+
+    const request = store.get(id);
+
+    request.onsuccess = function (event) {
+        const card = event.target.result;
+        if (card) {
+            card.Decorado = decorado;
+            const updateRequest = store.put(card);
+            updateRequest.onsuccess = function (event) {
+                console.log("Dados atualizados com sucesso!");
+            };
+            updateRequest.onerror = function (event) {
+                console.error("Erro ao atualizar dados:", event.target.error);
+            };
+        } else {
+            console.log("Dado não encontrado.");
+        }
+    };
+
+    request.onerror = function (event) {
+        console.error("Erro ao buscar dado para atualização:", event.target.error);
+    };
+}
+
+async function verificarDado(filtro) {
+    return new Promise((resolve, reject) => {
+        abrirBancoDeDados().then(db => {
+            const transaction = db.transaction(["words"], "readwrite");
+            const store = transaction.objectStore("words");
+
+            let request = null
+            switch (filtro.flag) {
+                case "id":
+                    request = store.get(filtro.id);
+                    break;
+                case "all":
+                    request = store.openCursor();
+                    break;
+                case "data":
+                    request = store.openCursor();
+                    break;
+                default:
+                    console.log("não vem ao caso");
+                    break;
+            }
+
+            let data_return = []
+            request.onsuccess = function (event) {
+                const request_result = event.target.result;
+
+                switch (filtro.flag) {
+                    case "data":
+                        if (request_result) {
+                            switch (filtro.not_key) {
+                                case false:
+                                    if (request_result.value[filtro.campo] == filtro.valor && request_result.value[filtro.campo] != undefined) {
+                                        data_return.push(request_result.value)
+                                    }
+                                    break;
+                                case true:
+                                    if (request_result.value[filtro.campo] == filtro.valor || request_result.value[filtro.campo] == undefined) {
+                                        data_return.push(request_result.value)
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                            request_result.continue();
+                        } else {
+                            resolve(data_return);
+                        }
+                        break;
+                    case "all":
+                        if (request_result) {
+                            data_return.push(request_result.value)
+                            request_result.continue();
+                        } else {
+                            resolve(data_return);
+                        }
+                        break;
+                    default:
+                        if (request_result) {
+                            //console.log("Dado encontrado:", request_result);
+                            resolve(request_result);
+                        } else {
+                            console.log("Dado não encontrado.");
+                        }
+                        break;
+                }
+            };
+
+            request.onerror = function (event) {
+                console.error("Erro ao verificar dado:", event.target.error);
+            };
+        })
+    })
+}
 
 if (window.innerWidth < window.innerHeight) {
     alert("Por favor, gire o dispositivo para a orientação horizontal para uma melhor experiência.");
@@ -10,12 +171,16 @@ if (window.innerWidth < window.innerHeight) {
 
 //NOTE - Load cards in window
 function insert_cards(greek_words) {
+    //console.log(greek_words);
     let html = ""
     function shuffleArray(inputArray) {
         inputArray.sort(() => Math.random() - 0.5);
     }
     sorted_words = shuffleArray(greek_words)
     ramdon_number = 0
+    if (!localStorage.getItem('greek_words'))
+        localStorage.setItem('greek_words', JSON.stringify(greek_words))
+
     for (const word of greek_words) {
         html += `<div data-json='${JSON.stringify(word)}' onclick="rotate_card(this)" id="card-${ramdon_number + 1}" class="card">
                     <img class="inferior-left-image" src="https://media-public.canva.com/mO-Oc/MAFaRVmO-Oc/1/tl.png" alt="">
@@ -30,35 +195,41 @@ function insert_cards(greek_words) {
                     <img class="superior-right-image" src="https://media-public.canva.com/mO-Oc/MAFaRVmO-Oc/1/tl.png" alt="">
                 </div>`
         ramdon_number += 1
-        //console.log(html);
     }
     card_container.innerHTML = html
+    //Verifica se aquele card está decorado
+    esta_decorado(atual_card_id)
 }
 //Abrir Json
-
-function filter_json(classification) {
-    fetch("greek_words.json")
-        .then(response => {
-            return response.json();
-        })
-        .then(jsondata => {
-            let filtradas = jsondata.filter((word) => word.Classificacao.includes(classification))
-            if (classification == "Todos") {
-                insert_cards(jsondata);
-            } else {
-                insert_cards(filtradas);
-            }
-        })
-        //ANCHOR - lembrar de transformar em função
-        location.href = `#card-1`
-        atual_card_id = 1
-        //Até aqui
+async function filter_json(classification) {
+    switch (classification) {
+        case "Todos":
+            dados_do_banco = await verificarDado({ flag: "all" });
+            break;
+        case "Não Decorados":
+            dados_do_banco = await verificarDado({ flag: "data", campo: "Decorado", valor: false, not_key: true });
+            //console.log("dado", dados_do_banco);
+            break;
+        case "Decorados":
+            dados_do_banco = await verificarDado({ flag: "data", campo: "Decorado", valor: true, not_key: false });
+            break;
+        default:
+            dados_do_banco = await verificarDado({ flag: "data", campo: "Classificacao", valor: classification, not_key: false });
+            //insert_cards(classification, dados_do_banco);
+            break;
+    }
+    insert_cards(dados_do_banco);
+    //ANCHOR - lembrar de transformar em função
+    location.href = `#card-1`
+    atual_card_id = 1
+    //Até aqui
 }
 fetch("greek_words.json")
     .then(response => {
         return response.json();
     })
     .then(jsondata => {
+        inserirDados(jsondata);
         insert_cards(jsondata);
     })
 
@@ -74,20 +245,20 @@ function rotate_card(card) {
     }
 }
 
-
 next.addEventListener('click', () => {
     atual_card_id += 1
     next.setAttribute("href", `#card-${atual_card_id}`)
+    esta_decorado(atual_card_id)
 });
 
 previus.addEventListener('click', () => {
     if (atual_card_id != 1)
         atual_card_id += - 1
     previus.setAttribute("href", `#card-${atual_card_id}`)
+    esta_decorado(atual_card_id)
 })
 
 document.addEventListener("keydown", (e) => {
-    //console.log(e.code);
     if (e.code == "ArrowRight" || e.code == "KeyD") {
         next.click()
     }
@@ -99,21 +270,53 @@ document.addEventListener("keydown", (e) => {
     }
 })
 
-function info_card(){
+function info_card() {
     card_data = JSON.parse(document.getElementById(`card-${atual_card_id}`).dataset.json)
-    delete card_data["Numero"]
+    delete card_data["id"]
     delete card_data["Traducao"]
     delete card_data["Vocabulo"]
     text = ""
     for (const k in card_data) {
-        text += `<div>${k}: ${card_data[k]}</div>`
+
+        if (k == "Casos") {
+            text += `<div>Nominativo: ${JSON.stringify(card_data[k].Nominativo)}</div>
+            <div>Genitivo: ${JSON.stringify(card_data[k].Genitivo)}</div>
+            <div>Dativo: ${JSON.stringify(card_data[k].Dativo)}</div>
+            <div>Acusativo: ${JSON.stringify(card_data[k].Acusativo)}</div>`
+        } else {
+            text += `<div>${k}: ${JSON.stringify(card_data[k])}</div>`
+        }
     }
 
     document.querySelector('.info-modal').innerHTML = text
-    
-    console.log();
 }
-//filtro de array
+
+//Marcar como Decorada
+
+async function foi_decorado() {
+    word_id = JSON.parse(document.getElementById(`card-${atual_card_id}`).dataset.json).id
+    //console.log(word_id);
+    dadoDB = await verificarDado({ flag: "id", id: word_id });
+    //console.log(dadoDB);
+    if (dadoDB.Decorado == true) {
+        atualizarDados(word_id, false)
+    } else {
+        atualizarDados(word_id, true)
+    }
+    esta_decorado(atual_card_id)
+}
+
+async function esta_decorado(atual_card_id) {
+    word_id = JSON.parse(document.getElementById(`card-${atual_card_id}`).dataset.json).id
+    //console.log(word_id);
+    dadoDB = await verificarDado({ flag: "id", id: word_id });
+    //console.log(dadoDB.Decorado);
+    if (dadoDB.Decorado == true) {
+        document.querySelector(".decorado").style.color = "green"
+    } else {
+        document.querySelector(".decorado").style.color = "red"
+    }
+}
 
 
 

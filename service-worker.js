ebc69594a07b636d29b2c683ac67f688665806bb
Piske <marcelohpiske@hotmail.com>
2024-04-12 20:54:15 -0300

    subindo service worker

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,64 @@
+// Nome do Service Worker
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('meu-app-cache')
         .then(function(cache) {
-          return buscarArquivosRecursivamente('/')
-            .then(function(arquivos) {
-              return cache.addAll(arquivos);
+          return cache.addAll([
+            '/',
+            '/index.html',
+            '/cards.html',
+            '/tables.html',
+            '/src/styles/index.css',
+            '/src/styles/cards.css',
+            '/src/styles/root.css',
+            '/src/styles/style.css',
+
+            '/src/scripts/cards.js',
+            '/src/scripts/script.js',
+            '/img/icon.png',
+            '/img/tl.png',
+          ]);
+        })
+    );
+  });
+  
+  // Interceptação de requisições de rede
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            // Retorna o recurso do cache se disponível
+            return response;
+          }
+          return fetch(event.request)
+            .then(function(response) {
+              // Armazena o novo recurso no cache
+              let armaz = response.clone()
+              caches.open('meu-app-cache')
+                .then(function(cache) {
+                  cache.put(event.request, armaz);
+                });
+              return response;
             });
         })
     );
   });
   
-  // Função para buscar arquivos recursivamente
-  async function buscarArquivosRecursivamente(caminho) {
-    const arquivos = [];
-    const diretorio = await caches.open('/');
-    const response = await diretorio.head(caminho);
+  // Notificação quando o Service Worker for atualizado
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys()
+        .then(function(cacheNames) {
+          return Promise.all(
+            cacheNames.map(function(cacheName) {
+              if (cacheName !== 'meu-app-cache') {
+                // Exclui caches antigos
+                return caches.delete(cacheName);
+              }
+            })
+          );
+        })
+    );
+  });
   
-    if (response.ok) {
-      if (response.headers.get('content-type').startsWith('text/')) {
-        arquivos.push(caminho);
-      }
-  
-      const subdiretorios = await response.text();
-      for (const subdiretorio of subdiretorios.split('\n')) {
-        if (subdiretorio.trim()) {
-          const subcaminho = `${caminho}${subdiretorio}/`;
-          const subarquivos = await buscarArquivosRecursivamente(subcaminho);
-          arquivos.push(...subarquivos);
-        }
-      }
-    }
-  
-    return arquivos;
-  }
